a1b8168646f7d779a6bf870a5a10526d5a1b6063
Bartek Łukawski <bwmn.peter@gmail.com>
2018-07-14 19:53:22 +0200

    Generate basic unit testing stub

---
+++ tests/testExampleLibrary.cpp
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+
+#include "ExampleLibrary.hpp"
+
+namespace roboticslab
+{
+
+/**
+ * @brief Tests \ref ExampleLibrary.
+ */
+class ExampleLibraryTest : public testing::Test
+{
+public:
+    virtual void SetUp()
+    {
+        exampleLibrary = new ExampleLibrary;
+    }
+
+    virtual void TearDown()
+    {
+        delete exampleLibrary;
+        exampleLibrary = 0;
+    }
+
+protected:
+    ExampleLibrary* exampleLibrary;
+};
+
+TEST_F(ExampleLibraryTest, ExampleLibraryConstructor)
+{
+    //ASSERT_TRUE(exampleLibrary->expectTrueResult());
+
+    ASSERT_TRUE(true);
+}
+
+}  // namespace roboticslab
